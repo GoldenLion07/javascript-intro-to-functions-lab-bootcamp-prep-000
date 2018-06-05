e2c289a63b1d3ef69628d579fb93fe20537c1be9
gmat191@gmail.com <gmat191@gmail.com>
2018-06-05 22:52:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ function whisper(string){
 }
 function logShout(string){
   console.log("Hello").toUpperCase
+  return string.toUpperCase
 }
 function logWhisper(string){
   console.log("Hello").toLowerCase
+  return string.toLowerCase
 }
 function sayHiToGrandma(string){
   return string()
