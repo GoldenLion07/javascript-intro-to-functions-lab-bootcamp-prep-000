7c4b50bdc06a5035aa03e5f36b007d8f22623e42
gmat191@gmail.com <gmat191@gmail.com>
2018-06-05 23:19:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   return(string.toLowerCase())
-  return(`${string}`.toUpperCase())
-  return(`${string}`.toLowerCase())
+  return(string.toUpperCase())
+  return(string.toLowerCase())
 }
