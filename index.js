cbd98f7505161272f582e13749d542443979a06b
gmat191@gmail.com <gmat191@gmail.com>
2018-06-06 00:37:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function sayHiToGrandma(string){
   if (string.toUpperCase() === string) {
     return("Yes Indeed!".toUpperCase());
   } 
-  if (string === "I love you, Grandma") {
+  if (string === "I love you, Grandma.") {
     return("I love you, too.")
   }
 }
