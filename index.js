dac35c6807b2e73b5d17db7f3cca2a2b8765b335
gmat191@gmail.com <gmat191@gmail.com>
2018-06-06 00:34:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string) {
-    console.log("I can't hear you!");
+    return("I can't hear you!");
   } 
   if (string.toUpperCase() === string) {
-    console.log("Yes Indeed".toUpperCase);
+    return("Yes Indeed".toUpperCase());
   } 
 }
